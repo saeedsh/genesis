81e5b685e95068a3bf709f5ec99880364893e547
Saeed <sd.shariati@gmail.com>
2014-03-17 17:42:38 -0300

    Both 32 and 64 bit are working

--- buildPackage.sh
+++ buildPackage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm -rf build-area  *.changes  *.tar.gz  *.deb  *.dsc  genesis
+rm -rf build-area  *.changes  *.tar.gz   *.dsc  genesis
 tar -cvzf genesis-2.3.tar.gz genesis-2.3
 bzr dh-make genesis 2.3 genesis-2.3.tar.gz
 rm genesis/debian/*.EX
